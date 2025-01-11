f105a70f3b55589215560c2a0a4d0b20db2f3834
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2025-01-11 16:09:50 -0600

    added ip to cors allowed origins (be)

--- DockerComposeBackend/settings.py
+++ DockerComposeBackend/settings.py
@@ -127,4 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Frontend development server
     "http://127.0.0.1:5173", # Alternate local address
+    "45.55.129.24:5173",
 ]
